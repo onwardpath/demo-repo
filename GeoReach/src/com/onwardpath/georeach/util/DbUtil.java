48872582fad0280b7667d1ab2e3ee1fa1c0e8e31
pandyan <pandyans@onwardpath.com>
2019-06-30 11:43:53 -0500

    Rewrote DBUtil to resolve database connection issue.

--- GeoReach/src/com/onwardpath/georeach/util/DbUtil.java
+++ GeoReach/src/com/onwardpath/georeach/util/DbUtil.java
@@ -1,48 +1,67 @@
 package com.onwardpath.georeach.util;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.sql.Timestamp;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.Timestamp;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class DbUtil {
-	  private static Connection dbConnection = null;
-
-	  public static Connection getConnection() {
-		  try {
-			  if (dbConnection == null || !dbConnection.isValid(1)) {		    	  		          		      		    
-		          InputStream inputStream = DbUtil.class.getClassLoader().getResourceAsStream("db.properties");
-		          Properties properties = new Properties();
-		          if (properties != null) {
-		              properties.load(inputStream);		
-		              String dbDriver = properties.getProperty("dbDriver");
-		              String connectionUrl = properties.getProperty("connectionUrl");
-		              String userName = properties.getProperty("userName");
-		              String password = properties.getProperty("password");			              
-		              //Class.forName(dbDriver).newInstance(); 		              		              
-		              dbConnection = DriverManager.getConnection(connectionUrl, userName, password);
-		          }	           
-			  }		      			 
-		  } catch (Exception e) {
-              e.printStackTrace();
-          }
-		  return dbConnection;
-	 }
-	  
-	 public static void closeConnection() {
-		  if (dbConnection != null) {
-			  try {
-				  dbConnection.close();
-				  dbConnection = null;
-			  } catch (Exception e) {
-				  e.printStackTrace();
-			  }			  
-	      }
-	 }
-	 
-	 public static String getTimestamp() {
-		 Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-	     return timestamp.toString();	 
-	 }
+	
+	private static DbUtil instance = new DbUtil();
+	public static final String DRIVER_CLASS = "com.mysql.jdbc.Driver";
+    private String URL, USER, PASSWORD = null;
+    
+    /*
+    dbDriver=com.mysql.jdbc.Driver
+	#connectionUrl=jdbc:mysql://localhost:3306/georeachdb
+	connectionUrl=jdbc:mysql://107.180.70.27:3306/georeachdb
+	userName=root
+	password=onwardAdmin1
+	*/
+		
+    private DbUtil() {       
+    }
+    
+    private void loadProperties() throws IOException {    	    		    	
+		InputStream inputStream = DbUtil.class.getClassLoader().getResourceAsStream("db.properties");
+        Properties properties = new Properties();
+        if (properties != null) {
+        	properties.load(inputStream);		
+        	//String dbDriver = properties.getProperty("dbDriver");
+        	URL = properties.getProperty("connectionUrl");
+        	URL += "?autoReconnect=true";
+        	USER = properties.getProperty("userName");
+        	PASSWORD = properties.getProperty("password");			              
+        	//Class.forName(dbDriver).newInstance(); 		              		                          
+        }		          	            
+    }
+    
+	private Connection createConnection() throws SQLException {
+		System.out.println(getTimestamp()+" @DbUtil.createConnection");
+		Connection connection = null;
+		connection = DriverManager.getConnection(instance.URL, instance.USER, instance.PASSWORD);
+	    return connection;
+	} 
+	
+	public static Connection getConnection() {
+		Connection connection = null;
+		System.out.println(getTimestamp()+" @DbUtil.getConnection");
+		try {
+			instance.loadProperties();
+			connection = instance.createConnection();
+		} catch (IOException ioe) {
+			System.out.println(ioe.getMessage());
+		} catch (SQLException sqle) {
+			System.out.println(sqle.getMessage());
+		}
+		return connection;
+	}
+	  	
+	public static String getTimestamp() {
+		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+		return timestamp.toString();	 
+	}
 }
