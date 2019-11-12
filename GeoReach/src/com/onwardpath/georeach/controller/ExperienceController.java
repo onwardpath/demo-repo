844ded5b66c7cc93d33c8cc41c970b374dc52925
Sremugaan <sre93mugan@gmail.com>
2019-11-12 21:07:41 +0530

    Remove strngutils import stmt

--- GeoReach/src/com/onwardpath/georeach/controller/ExperienceController.java
+++ GeoReach/src/com/onwardpath/georeach/controller/ExperienceController.java
@@ -4,8 +4,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.commons.lang.StringUtils;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
